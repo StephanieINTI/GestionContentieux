dbdd563869f023e03a60378593839c16c035079c
StephanieINTI <ansania971@gmail.com>
2019-08-30 11:44:49 +0200

    add Controller

---
+++ src/com/inti/controller/TribunauxController.java
@@ -0,0 +1,29 @@
+package com.inti.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import com.inti.entities.Tribunal;
+import com.inti.service.interfaces.ITribunalService;
+
+@RestController
+@CrossOrigin 
+public class TribunauxController {
+
+	@Autowired
+	ITribunalService tribunalService;
+	
+	
+	@RequestMapping(value = "/tibunal")
+	public List<Tribunal> findAllTribunal() {
+		return  tribunalService.findAll(Tribunal.class);
+		
+		
+	}
+}
