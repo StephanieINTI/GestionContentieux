cf970b8e5e7d6625a771fc42a77b204abc296195
unknown <anna.fall@outlook.com>
2019-09-02 15:19:14 +0200

    modifs post spring security

--- src/com/inti/dao/interfaces/IUtilisateurDAO.java
+++ src/com/inti/dao/interfaces/IUtilisateurDAO.java
@@ -1,12 +1,8 @@
 package com.inti.dao.interfaces;
 
-<<<<<<< HEAD
 import com.inti.entities.Utilisateur;
-=======
 
-public interface IUtilisateurDAO {
->>>>>>> master
-
-public interface IUtilisateurDAO extends IDAO<Utilisateur>{
-	Utilisateur findOneByUsername (String username);
+public interface IUtilisateurDAO extends IDAO<Utilisateur>  {
+	Utilisateur findOneByUsername(String username);
+	
 }
