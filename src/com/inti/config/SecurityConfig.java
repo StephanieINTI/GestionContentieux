1f75ad74dd516c2de4c015873e807b63467f8b8d
unknown <anna.fall@outlook.com>
2019-09-03 15:34:59 +0200

    modifs security config

--- src/com/inti/config/SecurityConfig.java
+++ src/com/inti/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -30,6 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(appUserDetailsService);
 	}
+	public void configure(WebSecurity web)throws Exception {
+		web.ignoring().antMatchers("/userAdmin/**", "/userAvocat/**", "/affaire/**", "/tribunal/**", "/tache/**");
+	}
+	
 	protected void configure(final HttpSecurity http) throws Exception {
 		http
 			.csrf().disable()
